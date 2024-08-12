64cf5af8f6730c018e01bb6c1b7386eb97c7d3d4
Damien Staebler <damienweifen@gmail.com>
2024-08-12 19:31:24 -0400

    version bump

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from kickapi import Kick, KickWebSockets, KickSaveSession
 from Plugin import Plugin
 
 class KickTP(Plugin):
-    __version__ = 10109
+    __version__ = 10200
 
     PLUGIN_ID = "com.github.killerboss2019.kicktp"
 
